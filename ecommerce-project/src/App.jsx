6a7dd46071280e6965d675b1f7122632ceff230b
venetia <venetiafaber@gmail.com>
2026-02-02 00:22:03 +0300

    Adds NotFound page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -3,6 +3,7 @@ import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import { OrdersPage } from './pages/OrdersPage';
 import { TrackingPage } from './pages/TrackingPage';
+import { NotFound } from './pages/NotFound';
 import './App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Route path="checkout" element={<CheckoutPage />} />
       <Route path="orders" element={<OrdersPage />} />
       <Route path="tracking" element={<TrackingPage />}/>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
---
+++ ecommerce-project/src/pages/NotFound.jsx
@@ -0,0 +1,16 @@
+import { Header } from '../components/Header';
+import './NotFound.css';
+
+export function NotFound() {
+  return(
+    <>
+      <Header />
+
+      <div className='not-found-page'>
+        <div className='page-title'>Sorry, the page that you are looking for, does not exist.
+
+        </div>
+      </div>
+    </>
+  );
+}

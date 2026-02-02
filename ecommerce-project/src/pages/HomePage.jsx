448f79d970dba6451c78adea03bdcf0913cf21ef
venetia <venetiafaber@gmail.com>
2026-02-02 18:08:21 +0300

    Uses data from the backend to generate the html

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
+import { useEffect, useState} from 'react';
 import { Header } from "../components/Header";
-import { products } from "../../starting-code/data/products";
 import Checkmark from "../assets/images/icons/checkmark.png";
 import "./HomePage.css";
 
@@ -24,10 +24,15 @@ export function HomePage() {
     });
   */
 
-  axios.get('http://localhost:3000/api/products')
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/products')
     .then((response) => {
-      console.log(response.data);
-    })
+      setProducts(response.data);
+    });
+  }, []);
+  
 
   
   return (

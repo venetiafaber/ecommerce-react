207effd805148fa2fbdd48d19bce1c24b781cf64
venetia <venetiafaber@gmail.com>
2026-02-05 14:28:10 +0300

    Converts home and checkout to async await

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -12,11 +12,11 @@ function App() {
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/cart-items?expand=product')
-      .then((response) => {
-        console.log(response.data);
-        setCart(response.data);
-      });
+    const fetchAppData = async () => {
+      const response = await axios.get('/api/cart-items?expand=product');
+      setCart(response.data);
+    };
+    fetchAppData();
   }, [])
 
   return (
--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -8,10 +8,11 @@ export function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/products')
-      .then((response) => {
-        setProducts(response.data);
-      });
+    const fetchData = async () => {
+      const response = await axios.get('/api/products');
+      setProducts(response.data);
+    }
+    fetchData();
   }, []);
 
   return (

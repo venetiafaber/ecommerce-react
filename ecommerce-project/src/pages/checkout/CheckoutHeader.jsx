1cafb3ced461b3f57c12c0c7022a8bcbf9e9370d
venetia <venetiafaber@gmail.com>
2026-02-08 16:07:45 +0300

    Displays total quantity in checkout header

--- ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
@@ -1,31 +1,37 @@
 import { Link } from "react-router";
-import Logo from '../../assets/images/logo.png';
-import MobileLogo from '../../assets/images/mobile-logo.png';
-import CheckoutLockIcon from '../../assets/images/icons/checkout-lock-icon.png';
+import Logo from "../../assets/images/logo.png";
+import MobileLogo from "../../assets/images/mobile-logo.png";
+import CheckoutLockIcon from "../../assets/images/icons/checkout-lock-icon.png";
 
-export function CheckoutHeader () {
-  return(
+export function CheckoutHeader({ cart }) {
+  let totalQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    totalQuantity += cartItem.quantity;
+  });
+
+  return (
     <div className="checkout-header">
-        <div className="header-content">
-          <div className="checkout-header-left-section">
-            <Link to="/">
-              <img className="logo" src={Logo} />
-              <img className="mobile-logo" src={MobileLogo} />
-            </Link>
-          </div>
+      <div className="header-content">
+        <div className="checkout-header-left-section">
+          <Link to="/">
+            <img className="logo" src={Logo} />
+            <img className="mobile-logo" src={MobileLogo} />
+          </Link>
+        </div>
 
-          <div className="checkout-header-middle-section">
-            Checkout (
-            <Link className="return-to-home-link" to="/">
-              3 items
-            </Link>
-            )
-          </div>
+        <div className="checkout-header-middle-section">
+          Checkout (
+          <Link className="return-to-home-link" to="/">
+            {totalQuantity} items
+          </Link>
+          )
+        </div>
 
-          <div className="checkout-header-right-section">
-            <img src={CheckoutLockIcon} />
-          </div>
+        <div className="checkout-header-right-section">
+          <img src={CheckoutLockIcon} />
         </div>
       </div>
+    </div>
   );
 }

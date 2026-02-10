e7e14aabb05686cf7de7f3448d20bd7ee2ebe92d
venetia <venetiafaber@gmail.com>
2026-02-10 11:31:40 +0300

    Displays the message 'Added' when adding to cart in the home page

--- ecommerce-project/src/pages/home/Product.jsx
+++ ecommerce-project/src/pages/home/Product.jsx
@@ -5,6 +5,7 @@ import Checkmark from "../../assets/images/icons/checkmark.png";
 
 export function Product({ product, loadCart }) {
   const [quantity, setQuantity] = useState(1);
+  const [isAdded, setIsAdded] = useState(false);
 
   const addToCart = async () => {
     await axios.post("/api/cart-items", {
@@ -12,6 +13,10 @@ export function Product({ product, loadCart }) {
       quantity,
     });
     await loadCart();
+    setIsAdded(true);
+    setTimeout(() => {
+      setIsAdded(false);
+    },2000);
   };
 
   const selectQuantity = (event) => {
@@ -60,7 +65,8 @@ export function Product({ product, loadCart }) {
 
       <div className="product-spacer"></div>
 
-      <div className="added-to-cart">
+      <div className="added-to-cart"
+        style={{ opacity: isAdded ? 1 : 0}}>
         <img src={Checkmark} />
         Added
       </div>

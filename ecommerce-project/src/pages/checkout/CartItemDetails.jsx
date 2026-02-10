b83d2dad1b839e37dfda01063d41dce99bade36c
venetia <venetiafaber@gmail.com>
2026-02-10 10:54:08 +0300

    Creates update cart item feature

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -1,10 +1,28 @@
 import axios from "axios";
+import { useState } from "react";
 import { formatMoney } from "../../utils/money";
 
 export function CartItemDetails({ cartItem, loadCart }) {
+  const [quantity, setQuantity] = useState(cartItem.quantity);
+  // let isUpdatingQuantity = true;
+
   const deleteCartItem = async () => {
     await axios.delete(`/api/cart-items/${cartItem.productId}`);
     await loadCart();
+  }
+
+  const selectQuantity = (event) => {
+    const quantitySelected = Number(event.target.value);
+    setQuantity(quantitySelected);
+  };
+
+  const updateCartItem = async () => {
+    // isUpdatingQuantity = true;
+    await axios.put(`/api/cart-items/${cartItem.productId}`, {
+      quantity
+    });
+    await loadCart();
+    // isUpdatingQuantity = false;
   }
 
   return (
@@ -19,9 +37,30 @@ export function CartItemDetails({ cartItem, loadCart }) {
         <div className="product-quantity">
           <span>
             Quantity:{" "}
-            <span className="quantity-label">{cartItem.quantity}</span>
+            {/* {!isUpdatingQuantity && <span className="quantity-label">{cartItem.quantity}</span>} */}
+            { 
+              // isUpdatingQuantity && 
+              <div className="product-quantity-container">
+                <select
+                  value={quantity}
+                  onChange={selectQuantity}
+                >
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+                </select>
+              </div>
+            }
           </span>
-          <span className="update-quantity-link link-primary">Update</span>
+          <span className="update-quantity-link link-primary"
+            onClick={updateCartItem}>Update</span>
           <span className="delete-quantity-link link-primary"
             onClick={deleteCartItem}
           >Delete</span>
